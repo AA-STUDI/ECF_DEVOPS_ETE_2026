e9b92d06c20a9bd231cc4b0082a718ffa13042aa
AA-STUDI <a.allart@levelup-cluster.eu>
2026-02-18 17:36:18 +0100

    syntax fix

--- 3_SpringBoot.sh
+++ 3_SpringBoot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd AT2_SpringBoot
-+chmod +x local_test.sh
+chmod +x local_test.sh
 ./local_test.sh
 if [ $? -ne 0 ]; then echo "ECHEC DU TEST LOCAL, DEPLOIEMENT ANNULE" ; cd .. ; exit 1 ; fi
 chmod +x deploy.sh
